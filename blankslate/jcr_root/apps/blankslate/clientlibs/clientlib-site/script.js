d639bfb270eac1c8a03a68f7cdb5747ad858d07e
Jim <jimfrenette@yahoo.com>
2019-09-07 16:48:35 -0400

    blankslate clientlibs added

---
+++ blankslate/jcr_root/apps/blankslate/clientlibs/clientlib-site/script.js
@@ -0,0 +1,15 @@
+
+
+/** IIFE to bootstrap the JS */
+
+(function () {
+    'use strict';
+
+    /* global var object */
+    window.blankslate = window.blankslate || {};
+
+    /* app */
+    blankslate.app = null;
+
+
+}).call(this);
